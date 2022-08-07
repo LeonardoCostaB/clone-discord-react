0d51ad4399941dd4843593aa5016d1b8ef27dc05
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-08-07 20:30:03 -0300

    criando butões de amigos, nitro e lista de amigos do usuario

--- src/components/SideBar/chat-option/index.tsx
+++ src/components/SideBar/chat-option/index.tsx
@@ -1,4 +1,10 @@
+import { Plus } from 'phosphor-react';
 import style from './style.module.scss';
+
+import friendsIcon from '../../../assets/svg/friends-icon.svg';
+import nitroIcon from '../../../assets/svg/nitro-icon.svg';
+import perfil01Img from '../../../assets/imgs/foto-perfil.jpg';
+import perfil02Img from '../../../assets/imgs/teste-perfil.jpg';
 
 export function ChatOption() {
     return (
@@ -10,6 +16,78 @@ export function ChatOption() {
                 >
                     Encontre ou comece uma conversa
                 </button>
+            </div>
+
+            <div className={style['container-buttonOptions']}>
+                <a 
+                    className={style['buttonOptions-friends']}
+                    href="#"
+                >
+                    <img src={friendsIcon} alt="Amigos" />
+                    
+                    Amigos
+                </a>
+
+                <a 
+                    className={style['buttonOptions-nitro']}
+                    href="#"
+                >
+                    <img src={nitroIcon} alt="Nitro" />
+
+                    Nitro
+                </a>
+            </div>
+
+            <div className={style['container-chatFriends']}>
+                <div className={style.title}>
+                    <h2>
+                        Mensagens diretas
+                    </h2>
+
+                    <button className={style.createMD} type='button'>
+                        <Plus
+                            className={style['createMD-icon']}
+                            color='#DCDDDE'
+                            weight='bold'
+                            cursor="pointer" 
+                            size={15}
+                        />
+                    </button>
+                </div>
+
+                <ul className={style['friends-list']}>
+                    <li>
+                        <a href="" className={style.friend}>
+                            <img
+                                src={perfil01Img}
+                                alt="foto de gustavo"
+                                className={style['photo-user']}
+                            />
+
+                            <span className={style['user-name']}>
+                                Gustavo
+                            </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="" className={style.friend}>
+                            <img
+                                src={perfil02Img}
+                                alt="foto de Pedro"
+                                className={style['photo-user']}
+                            />
+
+                            <span className={style['user-name']}>
+                                Pedro
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className={style['user-config']}>
+
             </div>
         </div>
     );
