960bff9c9528c53c41fd85fb2bd4f2c733b8aaeb
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-08-10 22:49:15 -0300

    estilizando botao de acesso a conversa e configuraçao do usuario

--- src/components/SideBar/chat-option/index.tsx
+++ src/components/SideBar/chat-option/index.tsx
@@ -3,8 +3,28 @@ import style from './style.module.scss';
 
 import friendsIcon from '../../../assets/svg/friends-icon.svg';
 import nitroIcon from '../../../assets/svg/nitro-icon.svg';
+import closeIcon from  '../../../assets/svg/close-icon.svg';
+import micIcon from  '../../../assets/svg/mic-icon.svg';
+import phoneIcon from  '../../../assets/svg/phone-icon.svg';
+import configIcon from  '../../../assets/svg/config-icon.svg';
+
 import perfil01Img from '../../../assets/imgs/foto-perfil.jpg';
 import perfil02Img from '../../../assets/imgs/teste-perfil.jpg';
+
+const configUser = {
+    mic: {
+        src: micIcon,
+        alt: "Clique aqui para desativar o microfone"
+    },
+    phone: {
+        src: phoneIcon,
+        alt: "Clique aqui para desativar todos os sons"
+    },
+    config: {
+        src: configIcon,
+        alt: "Clique aqui para acessar suas configurações"
+    }
+}
 
 export function ChatOption() {
     return (
@@ -61,12 +81,18 @@ export function ChatOption() {
                             <img
                                 src={perfil01Img}
                                 alt="foto de gustavo"
-                                className={style['photo-user']}
+                                className={'photo-user'}
                             />
 
                             <span className={style['user-name']}>
                                 Gustavo
                             </span>
+
+                            <img 
+                                src={closeIcon} 
+                                alt="Clique aqui para excluir essa mensagem" 
+                                className={style['delete-chat']}    
+                            />
                         </a>
                     </li>
 
@@ -75,19 +101,59 @@ export function ChatOption() {
                             <img
                                 src={perfil02Img}
                                 alt="foto de Pedro"
-                                className={style['photo-user']}
+                                className={'photo-user'}
                             />
 
                             <span className={style['user-name']}>
                                 Pedro
                             </span>
+
+                            <img 
+                                src={closeIcon} 
+                                alt="Clique aqui para excluir essa mensagem" 
+                                className={style['delete-chat']}    
+                            />
                         </a>
                     </li>
                 </ul>
             </div>
 
             <div className={style['user-config']}>
+                <div className={style['user-identification']}>
+                    <img 
+                        src={perfil01Img} 
+                        alt="Imagem de perfil Leonardo_costa"
+                        className={'photo-user'}
+                    />
+                    
+                    <div className={style['userName-container']}>
+                        <span className={style['name-identification']}>
+                            leonardo_c...
+                        </span>
+                        
+                        <span className={style['id']}>
+                            #9773
+                        </span>
+                    </div>
+                </div>
 
+                <div className={style['app-config']}>
+                    {
+                        Object.entries(configUser).map(([ key, value ]) => {
+                            return (
+                                <button 
+                                    key={key}
+                                    className={style["button-config"]}
+                                >
+                                    <img 
+                                        src={value.src} 
+                                        alt={value.alt} 
+                                    />
+                                </button>
+                            );
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
